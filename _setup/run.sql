a370ef308fa3352217bf78b9f4a3f05d75afc329
oodata-de <de.cloud2024@gmail.com>
2025-11-13 08:18:52 -0700

    Add multiple execute commands for DBT project

--- _setup/run.sql
+++ _setup/run.sql
@@ -1,3 +1,13 @@
 CREATE DBT PROJECT "DBT_DEV"."CONFIGS"."DBT_DEMO" FROM $$snow://workspace/USER$DECLOUD5.PUBLIC."dbt_demo_1"/versions/live/$$ DEFAULT_TARGET = 'dev'
 
 EXECUTE DBT PROJECT dbt_dev.configs.dbt_demo ARGS='run --select my_first_dbt_model --target dev';
+
+EXECUTE DBT PROJECT dbt_dev.configs.dbt_demo ARGS='run --select my_first_dbt_model --target dev';
+EXECUTE DBT PROJECT dbt_dev.configs.dbt_demo ARGS='run --select my_second_dbt_model --target dev';
+EXECUTE DBT PROJECT dbt_dev.configs.dbt_demo ARGS='run --target dev';
+
+ALTER DBT PROJECT "DBT_DEV"."CONFIGS"."DBT_DEMO" ADD VERSION FROM $$snow://workspace/USER$DECLOUD5.PUBLIC."dbt_demo_1"/versions/live/$$
+
+EXECUTE DBT PROJECT dbt_dev.configs.dbt_demo ARGS='run --select my_second_dbt_model --target dev';
+EXECUTE DBT PROJECT dbt_dev.configs.dbt_demo ARGS='run --target dev';
+
