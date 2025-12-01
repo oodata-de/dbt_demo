ef89e48306fe6e86e2f3fbd516c491669e8f03f2
Ojameruaye, Ovo <oojameru@bestbuycanada.ca>
2025-11-30 21:45:11 -0800

    update proc

--- orch_schedules.sql
+++ orch_schedules.sql
@@ -16,59 +16,51 @@ CREATE OR REPLACE TABLE sch_dbt_test.dbt_model_run_selector (
 
 -- Procedure that scans active selectors and creates/refreshes Snowflake tasks per selector.
 CREATE OR REPLACE PROCEDURE sch_dbt_test.sync_dbt_tasks(dbt_database STRING)
-RETURNS VARCHAR
-LANGUAGE SQL
+RETURNS STRING
+LANGUAGE JAVASCRIPT
 EXECUTE AS CALLER
 AS
 $$
-DECLARE
-    c_active CURSOR FOR
-        SELECT task_name,
-               task_schema,
-               run_selector,
-               run_type,
-               cron_expression,
-               dbt_project,
-               env
-        FROM IDENTIFIER(dbt_database || '.sch_dbt_test.dbt_model_run_selector')
-        WHERE is_active;
+const dbName = DBT_DATABASE;  // parameter auto-uppercased
 
-    v_task_name STRING;
-    v_task_schema STRING;
-    v_sql       STRING;
-    -- v_seen      STRING := '';
-    v_args      STRING;
-    v_resume    STRING;
-BEGIN
-    FOR rec IN c_active DO
-        v_task_name := 'DBT_TASK_' || rec.task_name;
-        v_task_schema := rec.task_schema;
+const selectorTable = `${dbName}.sch_dbt_test.dbt_model_run_selector`;
+const selectSql = `
+  SELECT task_name, task_schema, run_selector, run_type,
+         cron_expression, dbt_project, env
+  FROM ${selectorTable}
+  WHERE is_active
+`;
+const rs = snowflake.createStatement({ sqlText: selectSql }).execute();
 
-        IF (rec.run_type LIKE 'group-selector%') THEN
-            v_args := 'run --selector ' || rec.run_selector;
-        ELSE
-            v_args := 'run --select ' || rec.run_selector;
-        END IF;
-        v_args := v_args || ' --target ' || rec.env;
+while (rs.next()) {
+  const taskName   = 'DBT_TASK_' + rs.getColumnValue('TASK_NAME');
+  const taskSchema = rs.getColumnValue('TASK_SCHEMA');
+  const runType    = rs.getColumnValue('RUN_TYPE');
+  const selector   = rs.getColumnValue('RUN_SELECTOR');
+  const cron       = rs.getColumnValue('CRON_EXPRESSION');
+  const dbtProj    = rs.getColumnValue('DBT_PROJECT');
+  const env        = rs.getColumnValue('ENV');
 
-        v_sql := 'CREATE OR REPLACE TASK ' || dbt_database || '.' || v_task_schema || '.' || v_task_name || '
-                  WAREHOUSE = COMPUTE_WH
-                  SCHEDULE = ''' || rec.cron_expression || '''
-                  USER_TASK_TIMEOUT_MS = 3600000
-                  AS
-                  EXECUTE DBT PROJECT ' || rec.dbt_project || '
-                      ARGS = ''' || v_args || ''';';
+  const args = (runType.startsWith('group-selector')
+               ? `run --selector ${selector}`
+               : `run --select ${selector}`) + ` --target ${env}`;
 
-        EXECUTE IMMEDIATE v_sql;
+  const createTaskSql = `
+    CREATE OR REPLACE TASK ${dbName}.${taskSchema}.${taskName}
+      WAREHOUSE = COMPUTE_WH
+      SCHEDULE = '${cron}'
+      USER_TASK_TIMEOUT_MS = 3600000
+    AS
+      EXECUTE DBT PROJECT ${dbtProj}
+        ARGS = '${args}'
+  `;
+  snowflake.createStatement({ sqlText: createTaskSql }).execute();
 
-        v_resume := 'ALTER TASK ' || dbt_database || '.' || v_task_schema || '.' || v_task_name || ' RESUME;';
+  const resumeSql = `ALTER TASK ${dbName}.${taskSchema}.${taskName} RESUME`;
+  snowflake.createStatement({ sqlText: resumeSql }).execute();
+}
 
-        EXECUTE IMMEDIATE v_resume;
-        
-    END FOR;
-
-    RETURN 'Tasks created';
-END;
+return 'Tasks created';
 $$;
 
 
